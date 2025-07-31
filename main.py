8a8fb896aaf341c88a338b0f97a2aae82e0e378d
Samarth-1206 <pawar06samarth14@gmail.com>
2025-07-31 18:26:12 +0530

    Update main.py

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ app = Client(
 )
 
 if __name__ == "__main__":
-    print("𝙰𝚕𝚙𝚑𝚊 𝚂𝚎𝚜𝚜𝚒𝚘𝚗 𝙶𝚎𝚗 𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
+    print("ᴋᴀɪꜱᴇɴ 𝚂𝚎𝚜𝚜𝚒𝚘𝚗 𝙶𝚎𝚗 𝚜𝚝𝚊𝚛𝚝𝚒𝚗𝚐...")
     try:
         app.start()
     except ApiIdInvalid:
@@ -41,7 +41,7 @@ if __name__ == "__main__":
         raise
 
     uname = app.get_me().username
-    print(f"@{uname} NOW ALPHA SESSION GEN IS READY TO GEN SESSION")
+    print(f"@{uname} NOW ᴋᴀɪꜱᴇɴ SESSION GEN IS READY TO GEN SESSION")
     
     idle()
     
